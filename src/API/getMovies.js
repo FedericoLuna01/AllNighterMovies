2ef888c2d4f2cde856b1f4f0667fb3e975b7ddda
Federico Luna <federicolun4@gmail.com>
2023-03-16 21:14:45 -0300

    Fixed api url

--- src/API/getMovies.js
+++ src/API/getMovies.js
@@ -1,19 +1,19 @@
 export const getMovies = async (page = 1) => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_API_KEY}&page=${page}&language=es-AR`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=490927b5afb38df215e97d2db37d4f2a&page=${page}&language=es-AR`
   const response = await fetch(url)
   const data = await response.json()
   return data
 }
 
 export const getRecentMovies = async (page = 1) => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_API_KEY}&page=${page}&language=es-AR&sort_by=primary_release_date.desc`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=490927b5afb38df215e97d2db37d4f2a&page=${page}&language=es-AR&sort_by=primary_release_date.desc`
   const response = await fetch(url)
   const data = await response.json()
   return data
 }
 
 export const getSearchedMovies = async (query, page) => {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&page=${page}&language=es-AR&query=${query}`
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=490927b5afb38df215e97d2db37d4f2a&page=${page}&language=es-AR&query=${query}`
   const response = await fetch(url)
   const data = await response.json()
   return data
